3970b6b56ed001a266b5750faeb0d0c099195b97
Devroop Saha <130696540+devroopsaha744@users.noreply.github.com>
2024-05-18 18:43:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import blog_generate
+from fastapi import FastAPI
+from langserve import add_routes
+import uvicorn
+
+# LLM and chains to be used
+llm = blog_generate.llm
+chain = blog_generate.chain
+#rag_chain = blog_summarise.rag_chain
+
+app = FastAPI(title = 'blog-generation',
+              version = '1.8',
+              description = 'An API for blog generation')
+#Blog genreation
+add_routes(
+    app,
+    chain, 
+    path = "/blog-gen"
+)
+
+#Blog Summarization
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host = 'localhost', port = 8000)
